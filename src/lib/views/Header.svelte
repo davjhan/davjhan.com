e903ecc5d9b1f3ae10ef7f491b0092028ca2ee88
David Han <davidjhan96@gmail.com>
2022-01-02 13:07:20 -0800

    Fixed missing iCloud file.

---
+++ src/lib/views/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+    import ArrowDown20 from 'carbon-icons-svelte/lib/ArrowDown20'
+
+    let cl
+    export { cl as class }
+</script>
+<span class='inline-flex items-center font-semibold text-lg self-start {cl}'>
+    <ArrowDown20 class='inline mr-1'/><slot></slot>
+</span>
